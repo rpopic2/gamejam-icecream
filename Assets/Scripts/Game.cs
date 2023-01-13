704dc974cba7d06c4f859740c8546a59df2d4d89
rpopic2 <rpopic2@gmail.com>
2023-01-14 05:01:30 +0900

    tmp: mid save on changing cycle

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,30 +3,42 @@ using Rpopic.Window;
 
 public class Game : MonoBehaviour
 {
-    [SerializeField] private DayCounter _dayCounter;
+    private bool _isGameRunning = false;
+    [SerializeField] private GameObject _canvas;
+#nullable enable
+    private static GameObjectDict<Window>? _windows = null;
     private void Awake()
     {
+        if (_isGameRunning) return;
+        if (_windows is null) _windows = new(_canvas);
         if (!DontDestroyObject.IsLoaded) SceneLoader.LoadAdditive(SceneName.DontDestroy);
     }
     private void Start()
     {
+        if (_isGameRunning) return;
+        _isGameRunning = true;
+        Main();
+    }
+    private static void Main()
+    {
+        Day();
+        //TODO ReadyUp();
+    }
+    //TODO make async await
+    private static void Day()
+    {
         DayCounter.Instance.IncrementDay();
         var _timer = Timer.Instance;
-        _timer.OnTimerEnd = () => NextDay();
+        _timer.OnTimerEnd = () => ShowResult();
         _timer.SetTimer(2f);
         _timer.StartTimer();
-        
     }
-    private void NextDay()
+    private static void ShowResult()
     {
         AlertBox.Instance.Alert("End day", OnAnswer);
-        void OnAnswer(bool result) {
-            if (result) SceneLoader.Load(SceneName.Night);
+        void OnAnswer(bool _) {
+            _windows?["win_result"].Open();
         }
     }
 }
 
-public enum WeekDayName
-{
-    Mon, Tue, Wed, Thu, Fri, Sat, Sun
-}
--- Assets/Scripts/TextObjects/DayCounter.cs
+++ Assets/Scripts/TextObjects/DayCounter.cs
@@ -6,7 +6,6 @@ public class DayCounter : MonoBehaviour
     public static DayCounter Instance;
     private TMP_Text _dayText;
     private static int s_day = 0;
-    private static WeekDayName weekDay = WeekDayName.Sun;
     
     private void Awake()
     {
@@ -16,8 +15,6 @@ public class DayCounter : MonoBehaviour
     public void IncrementDay()
     {
         s_day += 1;
-        if (weekDay == WeekDayName.Sun) weekDay = WeekDayName.Mon;
-        else weekDay += 1;
         _dayText.text = $"DAY {s_day}";
     }
 }
