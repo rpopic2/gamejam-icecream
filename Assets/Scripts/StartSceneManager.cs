44cff03b60a51177141701707d65bfc7fc160df7
MakerJun46 <guswns6711@naver.com>
2023-01-14 22:07:22 +0900

    [Add] StartScene 추가

---
+++ Assets/Scripts/StartSceneManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartSceneManager : MonoBehaviour
+{
+    [SerializeField] Button Btn_start;
+    [SerializeField] Button Btn_preference;
+    [SerializeField] Button Btn_Eixt;
+
+    private void Awake()
+    {
+        Btn_start.onClick.AddListener(GameStart);
+        Btn_Eixt.onClick.AddListener(GameExit);
+    }
+
+    private void GameStart()
+    {
+        SceneLoader.Load(SceneName.Game);
+    }
+
+    private void GameExit()
+    {
+        Application.Quit();
+    }
+
+    private void OpenPreference()
+    {
+
+    }
+
+}
